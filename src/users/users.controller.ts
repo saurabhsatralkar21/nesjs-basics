1787786b2bc64705edeacbb43be04cb9ba0ad6dc
Saurabh S <saurabh.satralkar7@gmail.com>
2023-08-04 13:15:07 +0200

    Swagger for API testing installed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
     
